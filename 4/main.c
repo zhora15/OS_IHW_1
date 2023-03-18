caf7990d7edc6940c22542eed413b53a35177d6d
Georgy Senin <georgy.senin@gmail.com>
2023-03-19 01:28:36 +0300

    First version for 4 with header

---
+++ 4/main.c
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "../occurance.h"
+
+int main(int argc, char **argv) {
+    int fd1[2];
+    pid_t chpid1;
+
+    pipe(fd1);
+    chpid1 = fork();
+    if (chpid1 == -1) {
+        printf("An error occurred while creating first child\n");
+        exit(-1);
+    } else if (chpid1 == 0) {
+        int fd2[2];
+        pid_t chpid2;
+        pipe(fd2);
+        chpid2 = fork();
+        if (chpid2 == -1) {
+            printf("An error occurred while creating second child\n");
+            exit(-1);
+        } else if (chpid2 == 0) {
+            int file = open(argv[1], O_RDONLY);
+            char str[5001];
+            ssize_t n_bytes = read(file, str, 5000);
+            str[n_bytes++] = '\0';
+            close(fd2[0]);
+            write(fd2[1], &n_bytes, sizeof(ssize_t));
+            write(fd2[1], str, n_bytes);
+            exit(0);
+        } else {
+            close(fd2[1]);
+            char str[5001];
+            ssize_t len;
+            read(fd2[0], &len, sizeof(ssize_t));
+            read(fd2[0], str, len);
+            int ans[CMP_WORD];
+            memset(ans, 0, CMP_WORD * sizeof(int));
+            processString(str, ans);
+            close(fd1[0]);
+            write(fd1[1], ans, sizeof(ans));
+            exit(0);
+        }
+    } else {
+        close(fd1[1]);
+        int ans[CMP_WORD];
+        read(fd1[0], ans, sizeof(ans));
+        int file = open(argv[2], O_WRONLY);
+        char outp_str[100];
+        int outp_len = output_ans(ans, outp_str);
+        write(file, outp_str, outp_len);
+    }
+    return 0;
+}
